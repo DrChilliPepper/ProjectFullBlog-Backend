d732b2625f5ddb325e4d4ac54bb34281697acc55
DrChilliPepper <shuchu2004@gmail.com>
2025-12-16 23:29:13 +0530

    final

--- index.js
+++ index.js
@@ -13,13 +13,21 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express()
-app.use("/webhooks", webhookRouter)
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true
 }))
-app.use(express.json())
 app.use(clerkMiddleware())
+app.use("/webhooks", webhookRouter)
+app.use(express.json())
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use("/users", userRouter)
 app.use("/posts", postRouter)
 app.use("/comments", commentRouter)
